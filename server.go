1dc554e3d5c5dfe2ebc7ae4552572d3f8df03559
David Amako <davidamako06@gmail.com>
2023-10-25 15:40:06 +0100

    First Commit

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+	_ "strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func getUser(c echo.Context, db *sql.DB) error {
+	query, err := db.Query(`SELECT * FROM useraccounts`)
+
+	if err != nil {
+		panic(err.Error)
+	}
+
+	defer query.Close()
+
+	return c.JSON(http.StatusOK, db)
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.CORS())
+
+	db, err := sql.Open("mysql", "root:bball616.DAS@tcp(localhost:3306)/nea_db")
+
+	if err != nil {
+		panic(err.Error)
+	}
+
+	defer db.Close()
+
+	e.GET("/", func(c echo.Context) error {
+		return getUser(c, db)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
