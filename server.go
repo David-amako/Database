030340ce7a0a6abc932ea6681872e1b1e16ce81d
David Amako <davidamako06@gmail.com>
2023-10-25 17:12:29 +0100

    Second Commit

--- server.go
+++ server.go
@@ -10,6 +10,21 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Accounts struct {
+	Id               int    `json:"User_id"`
+	Firstname        string `json:"Firstname"`
+	Surname          string `json:"Surname"`
+	Email            string `json:"Email"`
+	Password         string `json:"Password"`
+	Registation_date string `json:"Registration_date"`
+	Address          string `json:"Address"`
+	Phone            string `json:"Phone"`
+}
+
+type UserData struct {
+	Users map[string]Accounts `json:"Users"`
+}
+
 func getUser(c echo.Context, db *sql.DB) error {
 	query, err := db.Query(`SELECT * FROM useraccounts`)
 
@@ -19,7 +34,25 @@ func getUser(c echo.Context, db *sql.DB) error {
 
 	defer query.Close()
 
-	return c.JSON(http.StatusOK, db)
+	data := UserData{Users: map[string]Accounts{}}
+
+	for query.Next() {
+		var name string
+
+		var User_id int
+
+		var firstname, surname, email, password, registation_date, address, phone string
+
+		err := query.Scan(&name)
+
+		if err != nil {
+			panic(err.Error)
+		}
+
+		data.Users[name] = Accounts{Id: User_id, Firstname: firstname, Surname: surname, Email: email, Password: password, Registation_date: registation_date, Address: address, Phone: phone}
+	}
+
+	return c.JSON(http.StatusOK, data)
 }
 
 func main() {
