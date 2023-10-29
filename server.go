b3ad04bb723dfd1aa59a96554c92672f8b9eb3ca
David Amako <davidamako06@gmail.com>
2023-10-29 21:02:22 +0000

    Sixth commit

--- server.go
+++ server.go
@@ -23,7 +23,7 @@ type Useraccount struct {
 }
 
 type Useraccounts struct {
-	Useraccounts []Useraccount `json:"useraccounts"`
+	Useraccounts map[string]Useraccount `json:"useraccounts"`
 }
 
 func main() {
@@ -37,7 +37,7 @@ func main() {
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 	}))
 
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/nea_db")
+	db, err := sql.Open("mysql", "root:bball616.DAS@tcp(localhost:3306)/nea_db")
 
 	if err != nil {
 		panic(err.Error)
@@ -63,92 +63,38 @@ func main() {
 }
 
 func getuserinfoAccount(c echo.Context) error {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/nea_db")
+	db, err := sql.Open("mysql", "root:bball616.DAS@tcp(localhost:3306)/nea_db")
 
-	requested_id := c.Param("id")
-	fmt.Println(requested_id)
-	//var firstname string
-	//var surname string
-	//var id string
-	var email string
-	var password string
-	//var registation_date string
-	//var address string
-	//var phone string
-
-	/*var sqlget string = ("SELECT email , Password FROM nea_db.useraccounts WHERE password = %s and email = %s;", , )*/
-
-	err = db.QueryRow("SELECT email , Password FROM nea_db.useraccounts").Scan(&email, &password)
-
+	query, err := db.Query("SELECT user_id, email , Password FROM nea_db.useraccounts")
 	if err != nil {
-		fmt.Println(err)
+		panic(err.Error)
 	}
 
-	response := Useraccount{Email: email, Password: password}
+	defer query.Close()
 
+	response := Useraccounts{Useraccounts: map[string]Useraccount{}}
+
+	for query.Next() {
+
+		//requested_id := c.Param("id")
+		//fmt.Println(requested_id)
+		//var firstname string
+		//var surname string
+		var id string
+		var email string
+		var password string
+		//var registation_date string
+		//var address string
+		//var phone string
+
+		err = (query).Scan(&id, &email, &password)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		response.Useraccounts[id] = Useraccount{Email: email, Password: password}
+
+	}
 	return c.JSON(http.StatusOK, response)
-}
-
-/*func createUserAccount(c echo.Context) error {
-	emp := new(Useraccount)
-	if err := c.Bind(emp); err != nil {
-		return err
-	}
-
-	db := c.Get("db").(*sql.DB)
-
-	// Insert data into the database
-	// Example MySQL insert statement:
-	insertStmt := "INSERT INTO `nea_db`.`useraccounts` (`Firstname`, `Surname`, `Email`, `Password`, `Registration_date`, `Address`, `Phone`) VALUES ('?', '?', '?', '?', '2023-10-27', '?', '?');"
-	stmt, err := db.Prepare(insertStmt)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-	defer stmt.Close()
-
-	result, err2 := stmt.Exec(emp.Firstname, emp.Surname, emp.Email, emp.Password, emp.Registation_date, emp.Address, emp.Phone)
-
-	if err2 != nil {
-		panic(err2)
-	}
-
-	fmt.Println(result.LastInsertId())
-
-	return c.JSON(http.StatusCreated, emp.Firstname)
-}*/
-
-/*func updateUserAccount(c echo.Context) error {
-	id := c.Param("id") // Get the user ID from the URL parameter
-	userAccount := new(Useraccount)
-	if err := c.Bind(userAccount); err != nil {
-		return err
-	}
-
-	db := c.Get("db").(*sql.DB)
-
-	// Update data in the database
-	// Example MySQL update statement:
-	updateStmt := "UPDATE useraccounts SET Firstname = ?, Surname = ?, Email = ?, Password = ?, Registration_date = ?, Address = ?, Phone = ? WHERE User_id = ?"
-	_, err := db.Exec(updateStmt, userAccount.Firstname, userAccount.Surname, userAccount.Email, userAccount.Password, userAccount.Registation_date, userAccount.Address, userAccount.Phone, id)
-	if err != nil {
-		return err
-	}
-
-	return c.JSON(http.StatusOK, userAccount)
-}*/
-
-func deleteUserAccount(c echo.Context) error {
-	id := c.Param("id") // Get the user ID from the URL parameter
-
-	db := c.Get("db").(*sql.DB)
-
-	// Delete the user account from the database
-	// Example MySQL delete statement:
-	deleteStmt := "DELETE FROM useraccounts WHERE User_id = ?"
-	_, err := db.Exec(deleteStmt, id)
-	if err != nil {
-		return err
-	}
-
-	return c.NoContent(http.StatusNoContent)
 }
